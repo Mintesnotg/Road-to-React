1d1892d947e04ed954c7c5e23f2d65a7f9cc91b1
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2024-01-04 11:11:35 +0300

    operand

--- Ternary/code.js
+++ Ternary/code.js
@@ -1,7 +1,14 @@
 const IsEven = (numb)=> {
 
+    
+
     return numb %2==0 ? true :false
 }
 
+const Falsy =(value)=> {
+
+    return value || 'welcome'
+}
+
  console.log( IsEven(5))
- 
+ console.log( Falsy(true))
