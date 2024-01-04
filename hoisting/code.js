d420d2f37be7ec218d64ba1d775f3431ac410f15
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2024-01-04 10:25:11 +0300

    Arrofucntionhoiting

---
+++ hoisting/code.js
@@ -0,0 +1,16 @@
+Hoisting()
+ArrowHoist()  // threw an error it should be decleard before it used
+
+
+
+
+
+
+function Hoisting () {
+
+    console.warn('warnning..')
+}
+
+const ArrowHoist = ()=>{
+    console.error('error ')
+}
